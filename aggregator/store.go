771af50a702bc96fa37f77acddc83d3197b4a1d8
Andrei Kozel <kozel.andrei.94@gmail.com>
2025-05-05 19:17:18 +0200

    impl store

--- aggregator/store.go
+++ aggregator/store.go
@@ -2,12 +2,17 @@ package main
 
 import "github.com/andrei-kozel/toll-calculator/types"
 
-type MemoryStrore struct{}
+type MemoryStrore struct {
+	data map[int]float64
+}
 
 func NewMemoryStore() *MemoryStrore {
-	return &MemoryStrore{}
+	return &MemoryStrore{
+		data: make(map[int]float64),
+	}
 }
 
 func (m *MemoryStrore) Insert(d types.Distance) error {
+	m.data[d.OBUID] += d.Value
 	return nil
 }
